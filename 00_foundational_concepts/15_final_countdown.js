4c52f58cca8da3cb75501e1c4715b3ee03f53152
Matt Desmet <mattd.email@gmail.com>
2017-04-05 19:00:00 -0700

    final update on foundations chapter algos

--- 00_foundational_concepts/15_final_countdown.js
+++ 00_foundational_concepts/15_final_countdown.js
@@ -7,6 +7,18 @@
 // Example: Given (3,5,17,9), print 6,12,15 (which are all of the multiples of 3 between 5
 // and 17, except for the value 9).
 
+// param1 - print the multiples of this param = 3
+// param2 - this is the starting value = 5
+// param3 - this is the ending value = 17
+// param4 - this is the exception.  Do NOT print if multiple is equal to this param value. = 9
+
 function getMultiples(param1,param2,param3,param4) {
-  
+  while (param2 < param3) {
+    if (param2 % param1 ==0 && param2 != param4) {
+      console.log(param2);
+    }
+    param2++
+  }
 }
+
+getMultiples(3,5,17,9)
