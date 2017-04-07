291974c29d1730c3cb8345bd506c4ddf233f8c6a
Matt Desmet <mattd.email@gmail.com>
2017-04-07 10:57:58 -0700

    adding more algos

--- 01_fundamentals/01_countdown.js
+++ 01_fundamentals/01_countdown.js
@@ -1,17 +1,17 @@
 // Create a function that accepts a number as an input. Return a new array that counts down by one,
 // from the number (as array’s ‘zero’th element) down to 0 (as the last element). How long is this array?
 
-var num = 10;
+// TEST: var num = 10; EPECTED OUTPUT = [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ]
 
 var returnSample = function countdown(num) {
   var arr = [];
-  while(num > -1);
-  arr.push(i);
-  num--;
+  var y = num
+  for (var i = y; i > -1; i--){
+    arr.push(i);
+  }
   return arr;
 }
 
-// countdown(num)
-// console.log(arr);
-// console.log(num);
-console.log(returnSample);
+var results = returnSample(10) // returns the value of arr at the actual array object
+console.log(results);
+console.log('The length of the array is: ', results.length);
