97331808083456a05ec699d65a6cd2c8d68f4614
Matt Desmet <mattd.email@gmail.com>
2017-04-08 09:19:46 -0700

    finished first 20

--- 01_fundamentals/20_negative_outlook.js
+++ 01_fundamentals/20_negative_outlook.js
@@ -3,16 +3,19 @@
 
 // iterate thru array.
 // check if value is already negative.  If so continue.
-// if positive, set value in temp variable and subtract temp from itself.  This gets the value to 0
-// subtract temp again to make the value negative.
+// if positive, set value in temp variable and subtract tempx2 from itself.
 
 var array = [1,-3,5]
 
 function makeNegative(array) {
+  console.log(array);
   for (var i = 0; i < array.length; i++){
     if (array[i] > 0 ){
       temp = array[i]
-      array[i]
+      array[i] = array[i] - (temp*2)
     }
   }
+  console.log(array);
 }
+
+makeNegative(array);
