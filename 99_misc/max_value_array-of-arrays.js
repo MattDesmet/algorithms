6ce415e748c14b0c7d5eed295650772eebd7afa7
Matt Desmet <mattd.email@gmail.com>
2017-04-20 19:58:58 -0700

    update

---
+++ 99_misc/max_value_array-of-arrays.js
@@ -0,0 +1,15 @@
+
+
+var array = [
+  [10,2,3,4],
+  [1,2,3,4],
+  [1,2,3,4],
+  [1,2,3,20],
+]
+
+function maxValArrayOfArrays(array) {
+  let max = (array[0])[0]
+  console.log(max);
+}
+
+maxValArrayOfArrays(array)
